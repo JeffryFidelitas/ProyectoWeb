e6b4213b81f323753c94d9877c4061a5d41e1f66
Jeni077 <105757830+Jeni077@users.noreply.github.com>
2024-07-14 23:15:38 -0400

    Uploaded html,css,js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function agregarTarea(tarea, lista) {
+    const elementoLi = document.createElement('li');
+    elementoLi.textContent = tarea;
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'Eliminar';
+    botonEliminar.addEventListener('click', () => {
+        lista.removeChild(elementoLi);
+    });
+    elementoLi.appendChild(botonEliminar);
+    lista.appendChild(elementoLi);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.getElementById('formulario');
+    const entrada = document.getElementById('entrada');
+    const lista = document.getElementById('lista');
+
+    formulario.addEventListener('submit', function(evento) {
+        evento.preventDefault();
+        const tarea = entrada.value.trim();
+        if (tarea !== '') {
+            agregarTarea(tarea, lista);
+            entrada.value = '';
+        }
+    });
+});
+
