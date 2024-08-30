7f1966e9b0ad9effc0560b4bff2037dc8d34a07c
Jeffry Samuel Eduarte Rojas <jeduarte30114@ufide.ac.cr>
2024-08-30 14:19:09 -0600

    Arreglado link a homepage de login

--- ingresar.php
+++ ingresar.php
@@ -20,7 +20,7 @@
     <h1>Orgánico CR</h1>
     <nav>
       <ul>
-        <li><a href="#">Inicio</a></li>
+        <li><a href="./">Inicio</a></li>
         <li><a href="nosotros">Nosotros</a></li>
         <li><a href="productos">Productos</a></li>
         <li><a href="servicios">Servicios</a></li>
